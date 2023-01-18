d6d15069a31b7daf2ab7cdb7b20d90049f6079c9
Kweeka <mtalbi29@yahoo.com>
2023-01-18 21:51:59 +0100

    Solve river sizes

--- challenge.js
+++ challenge.js
@@ -1,6 +1,37 @@
-const riverSizes = () => {
-  // Your code here
+const traverse = (arr, visited, i, j) => {
+  if (i >= arr.length || i < 0) return 0;
+  if (j >= arr[i].length || j < 0) return 0;
+  let size = 0
+
+  if (arr[i][j] === 1 && !visited[i][j]) {
+    size++
+    visited[i][j] = true
+    size += traverse(arr, visited, i + 1, j)
+    size += traverse(arr, visited, i - 1, j)
+    size += traverse(arr, visited, i, j + 1)
+    size += traverse(arr, visited, i, j - 1)
+    return size
+  }
+
+  return 0;
 }
+
+const riverSizes = (arr) => {
+  const sizes = [];
+  const visited = Array.from(Array(arr.length), () => null)
+    .map(() => Array.from(Array(arr[0].length), () => false))
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const size = traverse(arr, visited, i, j);
+      if (size > 0) sizes.push(size);
+    }
+  }
+
+  return sizes;
+}
+
+
 
 // const river1 = [
 //   [0, 0, 0, 1],
@@ -8,6 +39,8 @@ const riverSizes = () => {
 //   [1, 0, 0, 1],
 //   [1, 0, 0, 1],
 // ];
+
+// console.log(riverSizes(river1))
 
 // const river2 = [
 //   [0, 0, 0, 1, 1],
