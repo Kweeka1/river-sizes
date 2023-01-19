e0034c73c7183f082e49ebe558c7882dbc9f3e86
kweeka <mtalbi29@yahoo.com>
2023-01-19 08:24:46 +0100

    Solve river sizes

--- challenge.js
+++ challenge.js
@@ -1,5 +1,33 @@
-const riverSizes = () => {
-  // Your code here
+const traverse = (arr, visited, i, j) => {
+  if (i >= arr.length || i < 0) return 0
+  if (i >= arr[0].length || i < 0) return 0
+  let size = 0;
+
+  if (arr[i][j] === 1 && !visited[i][j]) {
+    size++
+    visited[i][j] = true;
+    size += traverse(arr, visited, i + 1, j)
+    size += traverse(arr, visited, i - 1, j)
+    size += traverse(arr, visited, i, j + 1)
+    size += traverse(arr, visited, i, j - 1)
+    return size;
+  }
+
+  return 0;
+}
+
+const riverSizes = (arr) => {
+  const sizes = [];
+  const visited = Array.from(Array(arr.length), () => Array.from(Array(arr[0].length), () => false))
+  
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const size = traverse(arr, visited, i, j);
+      if (size > 0) sizes.push(size);
+    }
+  }
+
+  return sizes;
 }
 
 // const river1 = [
@@ -8,6 +36,8 @@ const riverSizes = () => {
 //   [1, 0, 0, 1],
 //   [1, 0, 0, 1],
 // ];
+
+// console.log(riverSizes(river1))
 
 // const river2 = [
 //   [0, 0, 0, 1, 1],
@@ -21,5 +51,9 @@ const riverSizes = () => {
 
 // riverSizes(river2)
 // Output: [6, 2]
+
+// write 2 for loops
+// make a copy of the same array with false as values
+
 
 module.exports = riverSizes;
